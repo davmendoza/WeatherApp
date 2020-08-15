ded7e9b1b6e4fd143aea899f78c2cd1b8fc48536
david mendoza <davidsmacbook@davids-mbp.attlocal.net>
2020-08-15 07:13:43 -0700

    updated files

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-$(document).ready(function(){
-    $('#submitWeather').click(function(){
-        
-        var city = $('#city').val();
-        var state = $('#state').val();
+$(document).ready(function () {
+// var city = "san diego"
+// var APIKey = "7f1dcb44e7dbc65fc6e5097ebb0f6a06";
+// var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial" + "&appid=" + APIKey;
 
-        if(city != ''){
+// $.ajax({
+//     type: "method",
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(queryURL)
+//     console.log(response)
+//     $('.city').text(response.city.name);
+//     $('.temp').text(JSON.stringify(response.list[0].main.temp));
+//     $('.humidity').text(response.main.humidity);
+//     $('.wind').text(temp);
 
-            $.ajax({
+// });
 
-                url:'http://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial" + "&APPID=7f1dcb44e7dbc65fc6e5097ebb0f6a06",
-                method: "GET",
-                //dataType: "jsonp"
 
-            }).then(function(response){
-                
+//function searchButton() {
+//     $("#submitWeather").on('click', function(event) {
 
-            });
+//         const searchBtn 
 
-        }else{
-            $("error").html('This cannot be empty')
-        } 
-
-    });
-
+//     });
+// }
 });
-
-function display (response) {  
-    return "<h2><"
-}
