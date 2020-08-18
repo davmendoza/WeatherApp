7084b83f94318c5a86b40b05391ff2aca26f4cdf
david mendoza <davidsmacbook@davids-mbp.attlocal.net>
2020-08-18 16:43:23 -0700

    fixed styling amd java

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-$(document).ready(function () {
+
+//function currentWeather(
 var city = "san diego";
-var APIKey = "7f1dcb44e7dbc65fc6e5097ebb0f6a06";
-var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial" + "&appid=" + APIKey;
+//var APIKey = "7f1dcb44e7dbc65fc6e5097ebb0f6a06";
+var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "&units=imperial&appid=7f1dcb44e7dbc65fc6e5097ebb0f6a06";
 // var searchButton = $('#submitWeather');
 // var citySearchInput = $('#city').val();
 
+//current weather data
 $.ajax({
     type: "method",
     url: queryURL,
@@ -13,15 +15,15 @@ $.ajax({
     console.log(queryURL)
     console.log(response)
     $('.city').text(response.city.name);
-    $('.temp').text(JSON.stringify(response.list[0].main.temp));
+    $('.temp').text("Temperature: " + response.list[0].main.temp);
     $('.humidity').text("Humidity: " + response.list[0].main.humidity);
-    $('.wind').text(JSON.stringify(response.list[0].wind.speed));
-    $('.weather-date').text(JSON.stringify(response.list[0].dt_txt));
-    $('.sun').text(JSON.stringify(response.list[2].main.temp));
-    $('.mon').text(JSON.stringify(response.list[10].main.temp));
-    $('.tues').text(JSON.stringify(response.list[18].main.temp));
-    $('.wed').text(JSON.stringify(response.list[26].main.temp));
-    $('.thurs').text(JSON.stringify(response.list[34].main.temp));
+    $('.wind').text("Wind Speed: " + response.list[0].wind.speed);
+    $('.weather-date').text(response.list[0].dt_txt);
+    $('.sun').text(response.list[2].main.temp);
+    $('.mon').text(response.list[10].main.temp);
+    $('.tues').text(response.list[18].main.temp);
+    $('.wed').text(response.list[26].main.temp);
+    $('.thurs').text(response.list[34].main.temp);
     
     
 
@@ -36,5 +38,6 @@ $.ajax({
 //        
 
 //     });
-}
+$(document).ready(function () {
+});
 
